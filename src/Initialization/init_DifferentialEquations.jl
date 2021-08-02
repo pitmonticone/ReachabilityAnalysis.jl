dbc28b11ff9d4e4aa45abe1f6f8d28a37f8a8ef0
schillic <git@christianschilling.net>
2021-08-02 14:58:51 +0200

    allow to choose initial states in _solve_ensemble

--- src/Initialization/init_DifferentialEquations.jl
+++ src/Initialization/init_DifferentialEquations.jl
@@ -7,6 +7,7 @@ function _solve_ensemble(ivp::InitialValueProblem, args...;
                          trajectories_alg=DE.Tsit5(),
                          ensemble_alg=DE.EnsembleThreads(),
                          inplace=true,
+                         initial_states=nothing,
                          kwargs...)
 
     # get problem's vector field
@@ -20,17 +21,22 @@ function _solve_ensemble(ivp::InitialValueProblem, args...;
     dt = _get_tspan(args...; kwargs...)
     tspan = (inf(dt), sup(dt))
 
-    # sample initial states
-    X0 = initial_state(ivp)
-    sampler = get(kwargs, :sampler, LazySets._default_sampler(X0))
-    rng = get(kwargs, :rng, LazySets.GLOBAL_RNG)
-    seed = get(kwargs, :seed, nothing)
-    include_vertices = get(kwargs, :include_vertices, false)
-    X0_samples = sample(X0, trajectories; sampler=sampler, rng=rng, seed=seed, include_vertices=include_vertices)
+    if isnothing(initial_states)
+        # sample initial states of the IVP
+        X0 = initial_state(ivp)
+        sampler = get(kwargs, :sampler, LazySets._default_sampler(X0))
+        rng = get(kwargs, :rng, LazySets.GLOBAL_RNG)
+        seed = get(kwargs, :seed, nothing)
+        include_vertices = get(kwargs, :include_vertices, false)
+        initial_states = sample(X0, trajectories; sampler=sampler, rng=rng,
+                                seed=seed, include_vertices=include_vertices)
+    else
+        trajectories = length(initial_states)
+    end
 
     # formulate ensemble ODE problem
-    ensemble_prob = ODEProblem(field, first(X0_samples), tspan)
-    _prob_func(prob, i, repeat) = remake(prob, u0 = X0_samples[i])
+    ensemble_prob = ODEProblem(field, first(initial_states), tspan)
+    _prob_func(prob, i, repeat) = remake(prob, u0 = initial_states[i])
 
     # choose tolerances
     reltol = get(kwargs, :reltol, 1e-3)
